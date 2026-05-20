e457c7b60b7a471b05c81b8c5cde6a0ca5322e21
Yoga Sandhy Winata <yogas5381@gmail.com>
2026-05-20 10:41:31 +0700

    test: Add widget test for adding a service item

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:manajemen_pelayanan/main.dart';
 
@@ -7,5 +8,23 @@ void main() {
 
     expect(find.text('Manajemen Pelayanan'), findsOneWidget);
     expect(find.text('Masuk'), findsOneWidget);
+  });
+
+  testWidgets('can add a service item', (tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    await tester.tap(find.text('Masuk'));
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('Tambah Layanan'));
+    await tester.pumpAndSettle();
+
+    await tester.enterText(find.byType(EditableText).at(0), 'Tes Layanan');
+    await tester.enterText(find.byType(EditableText).at(1), 'Keterangan tes');
+    await tester.tap(find.text('Simpan'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Tes Layanan'), findsOneWidget);
+    expect(find.text('Keterangan tes'), findsOneWidget);
   });
 }
